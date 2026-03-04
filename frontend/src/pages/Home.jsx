8e31fd923a3aa7c2599e3c43545a370911f14fcb
geetam <geetambinani6@gmail.com>
2026-03-04 20:56:53 +0530

    added borders

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   const [panelOpen, setPanelOpen] = useState(false);
   const panelRef = useRef(null);
   const panelCloseRef = useRef(null);
-  const panelVehicleRef = useRef(null); 
+  const panelVehicleRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,14 +22,13 @@ const Home = () => {
         height: "70%",
         opacity: 1,
         padding: 20,
-
       });
       gsap.to(panelCloseRef.current, {
         opacity: 1,
       });
       gsap.to(panelVehicleRef.current, {
         opacity: 1,
-        position:"fixed",
+        position: "fixed",
       });
     } else {
       gsap.to(panelRef.current, {
@@ -42,8 +41,7 @@ const Home = () => {
       });
       gsap.to(panelVehicleRef.current, {
         opacity: 0,
-        position:"unset",
-
+        position: "unset",
       });
     }
   }, [panelOpen]);
@@ -97,9 +95,12 @@ const Home = () => {
         </div>
       </div>
 
-      <div ref={panelVehicleRef} className="bottom-0 opacity-0 z-10 px-3 py-6 bg-white w-full">
+      <div
+        ref={panelVehicleRef}
+        className="bottom-0 opacity-0 z-10 px-3 py-6 bg-white w-full"
+      >
         <h3 className="text-2xl font-semibold mb-5"> Choose a Vehicle Type</h3>
-        <div className="flex border-2 border-black rounded-xl items-center p-3 justify-between w-full mb-2">
+        <div className="flex active:border-black active:border-2   rounded-xl items-center p-3 justify-between w-full mb-2">
           <img
             src="https://th.bing.com/th/id/OIP.2SyVm_Yz4l2O0Nz_upaCFgHaF7?w=126&h=108&c=7&qlt=90&bgcl=a29f76&r=0&o=6&dpr=1.3&pid=13.1"
             alt="car image"
@@ -122,7 +123,7 @@ const Home = () => {
           </div>
           <h2 className="text-xl font-semibold">₹193.20</h2>
         </div>
-         <div className="flex border-2 border-black rounded-xl items-center p-3 justify-between w-full mb-2">
+        <div className="flex  active:border-black active:border-2  rounded-xl items-center p-3 justify-between w-full mb-2">
           <img
             src="https://th.bing.com/th/id/OIP.eQB0gi41q925wUMFN6QieQHaHk?w=176&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3"
             alt="motorcycle"
@@ -145,7 +146,7 @@ const Home = () => {
           </div>
           <h2 className="text-xl font-semibold">₹65</h2>
         </div>
-          <div className="flex border-2 border-black rounded-xl items-center p-3 justify-between w-full mb-2">
+        <div className="flex active:border-black active:border-2    rounded-xl items-center p-3 justify-between w-full mb-2">
           <img
             src="https://th.bing.com/th/id/OIP.h989F5_2QghpWh2kea_NkQHaHE?w=149&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3"
             alt="auto"
@@ -157,7 +158,7 @@ const Home = () => {
               <span>
                 <i className="ri-user-3-fill"></i>
               </span>
-              1
+              3
             </h4>
             <h5 className="font-medium text-sm">
               10 mins away · Arrives 3.20 PM
